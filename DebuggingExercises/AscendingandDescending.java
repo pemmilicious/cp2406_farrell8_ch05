5f7c59a77d8f439d04804733818e82cb0e367005
Bradley <bradley.pemmelaar@my.jcu.edu.au>
2016-08-26 15:16:25 +1000

    chap 5

--- DebuggingExercises/AscendingandDescending.java
+++ DebuggingExercises/AscendingandDescending.java
@@ -3,28 +3,58 @@
  */
 public class AscendingandDescending {
     public static void main(String args[]) {
-        int num1 = 17;
-        int num2 = 3;
-        int num3 = 14
-        int low, high;
-
+        int num1 = 12;
+        int num2 = 18;
+        int num3 = 15;
+        int low, med, high;
+        if(num1 <= num2 && num1 <= num3)
         {
             low = num1;
-            if(num2 <= num1)
+            if(num2 <= num3)
             {
-                low = num2;
-                high = num1;
+                med = num2;
+                high = num3;
             }
             else
             {
-                low = num1;
+                med = num3;
                 high = num2;
             }
         }
+        else
         {
-            high = num3        }
-        System.out.println(low);
-        System.out.println(high);
+            if(num2 <= num1 && num2 <= num3)
+            {
+                low = num2;
+                if(num1 <= num3)
+                {
+                    med = num1;
+                    high = num3;
+                }
+                else
+                {
+                    med = num3;
+                    high = num1;
+                }
+            }
+            else
+            {
+                low = num3;
+                if(num1 <= num2)
+                {
+                    med = num1;
+                    high = num2;
+                }
+                else
+                {
+                    med = num2;
+                    high = num1;
+                }
+            }
+        }
+            System.out.println(low);
+            System.out.println(med);
+            System.out.println(high);
 
+        }
     }
-}
